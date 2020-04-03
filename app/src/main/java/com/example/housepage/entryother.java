1b59009abd0026c551b67779276efa45e29b221f
Ashitha132 <ashithaom132@gmail.com>
2020-04-03 15:36:24 -0400

    new updates

--- app/src/main/java/com/example/housepage/entryother.java
+++ app/src/main/java/com/example/housepage/entryother.java
@@ -6,14 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class entryother extends AppCompatActivity {
     Button sub;
+    Spinner water;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entryother);
+        water=(Spinner)findViewById(R.id.spinnerwater);
+        final RadioGroup rainradio=(RadioGroup)findViewById(R.id.rain);
+        final RadioGroup waste=(RadioGroup)findViewById(R.id.waste);
+        final RadioGroup farm=(RadioGroup)findViewById(R.id.farm);
+        final RadioGroup scarcity=(RadioGroup)findViewById(R.id.scarcity);
+
+
 
         sub=(Button)findViewById(R.id.subdetails);
         sub.setOnClickListener(new View.OnClickListener() {
